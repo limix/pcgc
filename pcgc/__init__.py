8883f1f5d36230f0c171efcad7195f2a43d05104
Danilo Horta <danilo.horta@gmail.com>
2018-04-24 11:50:38 +0100

    first commit

---
+++ pcgc/__init__.py
@@ -0,0 +1,5 @@
+from .pcgc import pcgc
+
+__version__ = "0.0.1"
+
+__all__ = ['__version__', 'pcgc']
---
+++ pcgc/pcgc.py
@@ -0,0 +1,22 @@
+from __future__ import division
+
+from brent_search import brent
+import numpy as np
+import scipy.stats as st
+
+
+def pcgc_cost(y, kinship, c, h2):
+    cost = 0
+    n = len(y)
+    for i in range(0, len(y) - 1):
+        d = y[i] * y[i + 1:] - c * h2 * kinship[i, i + 1:]
+        cost += np.sum(d**2) / n
+    return cost
+
+
+def pcgc(y, kinship, K):
+    y = (y - np.mean(y)) / np.std(y)
+    P = np.sum(y > 0) / len(y)
+    t = st.norm.isf(K)
+    c = P * (1 - P) * st.norm.pdf(t)**2 / (K**2 * (1 - K)**2)
+    return brent(lambda h2: pcgc_cost(y, kinship, c, h2), 1e-4, 1 - 1e4)[0]
